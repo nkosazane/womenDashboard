d10f93808459da83d0e89de12ed6ceee5f66585d
GugEnhle <ntombenhlenuh@yahoo.com>
2019-10-28 15:59:10 +0200

    Style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,8 @@
+import { DonationsComponent } from './component/donations/donations.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { ChatsComponent } from './component/chats/chats.component';
+import { UserProfileComponent } from './component/user-profile/user-profile.component';
+import { TablesComponent } from './component/tables/tables.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './component/login/login.component';
@@ -8,7 +13,16 @@ import { AuthGuardService } from './service/auth-guard.service';
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full'},
   { path: 'login', component: LoginComponent},
-  { path: 'home', component:HomeComponent}
+  { path: 'home', component:HomeComponent},
+  {path: 'tables',component:TablesComponent},
+  {path: 'user-profile',component:UserProfileComponent},
+  {path: 'chats',component:ChatsComponent},
+  {path: 'dashboard',component:DashboardComponent},
+  {path: 'donations',component:DonationsComponent}
+
+
+
+
 ];
 
 @NgModule({
