8579c323a1088fb54ea5fef07b39e48847b0d214
gontsejnr <gontsemaepa@gmail.com>
2019-10-28 11:27:40 +0200

    .

--- src/app/service/admin.service.ts
+++ src/app/service/admin.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestoreDocument, AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
+  private userDoc: AngularFirestoreDocument<Users>
+  private donationDoc: AngularFirestoreDocument<Donation>
+  private periodsDoc: AngularFirestoreDocument<Periods>
 
-  constructor() { }
+  constructor(private angularfire: AngularFirestore) { }
+
+  //get all users from the database
+  getUsers(){
+    return this.angularfire.collection('users').snapshotChanges();
+  }
+
+  getDonations(){
+    return this.angularfire.collection('donation').snapshotChanges();
+  }
+
+  getPeriods(){
+    return this.angularfire.collection('tracker').snapshotChanges();
+  }
 }
